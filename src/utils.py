63eef8eacfcbddc3d7048de3fb1fa46a9c867ea0
Sean <chiseanchang0727@gmail.com>
2024-11-08 11:12:52 +0800

    wip

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,17 @@
-def load_environment():
-    pass
+import os
+import pandas as pd
+
+
+def load_dataframes(folder_path) -> pd.DataFrame:
+    dataframes = pd.DataFrame()
+    
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.csv'):  
+            device_name = filename.split('_')[0]
+            df_name = "df_" + device_name
+            df = pd.read_csv(os.path.join(folder_path, filename))
+            df['device'] = device_name
+            
+            dataframes = pd.concat([dataframes, df], axis=0)
+
+    return dataframes
