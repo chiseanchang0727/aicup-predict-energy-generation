8cf4f82bafe4da1114d07825368c2811269ccb6c
chiseanchang0727 <luciuskyear0812@gmail.com>
2024-11-14 21:27:42 +0800

    rm redundant

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import sys
 import numpy as np
 import pandas as pd
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import TimeSeriesSplit
 
 # Make `src` dir can be imported
