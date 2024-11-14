14b9fa6779477d8b11c9a8c745851268b5602cb0
chiseanchang0727 <luciuskyear0812@gmail.com>
2024-11-14 21:26:13 +0800

    rm redundant

--- main.py
+++ main.py
@@ -30,38 +30,13 @@ pe_config = configs['pe_config']
 
 ######################################################################
 
-
-day_gap = 24 * 60
 raw_data_path = "data/processed_data/combined_data.csv"
-
 
 def read_raw_data_and_sort(raw_data_path: str, sort_by_cols: list = ["device", "datetime"]) -> pd.DataFrame:
     df_raw_data = pd.read_csv(raw_data_path, parse_dates=["datetime"])
     # make sure the sorting is correct
     df_raw_data = df_raw_data.sort_values(by=sort_by_cols).reset_index(drop=True)
     return df_raw_data
-
-
-def split_train_test(df, plot=False):
-    _, axs = plt.subplots(5, 1, figsize=(15, 15), sharex=True)
-
-    fold = 0
-    for train_idx, val_idx in tss.split(df):
-        df_train = df.iloc[train_idx]
-        df_test = df.iloc[val_idx]
-
-        if plot:
-            df_train["power"].plot(
-                ax=axs[fold],
-                label="Training Set",
-                title=f"Data Train/Test Split Fold {fold}",
-            )
-            df_test["power"].plot(ax=axs[fold], label="Test Set")
-            axs[fold].axvline(df_test.index.min(), color="black", ls="--")
-
-            fold += 1
-
-    return df_train, df_test
 
 def get_test_size(days):
     # since the data is one minute one row
